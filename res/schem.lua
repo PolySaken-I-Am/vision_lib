13469740604c794c6457db2a29e5ca2ece2dd4e3
PolySaken <37032857+PolySaken-I-Am@users.noreply.github.com>
2020-07-08 12:45:03 +1200

    connected block collector now 3d

--- res/schem.lua
+++ res/schem.lua
@@ -37,6 +37,20 @@ function visionLib.Schem.GetConnected(pos, acceptable)
 				table.insert(l2, visionLib.Table.Clone(npos))
 				con=con+1
 			end
+			local npos = visionLib.Table.Clone(v)
+			npos.y=npos.y+1
+			if acceptable[minetest.get_node(npos).name] and not stc[npos.x.." "..npos.y.." "..npos.z] then
+				stc[npos.x.." "..npos.y.." "..npos.z]=visionLib.Table.Clone(npos)
+				table.insert(l2, visionLib.Table.Clone(npos))
+				con=con+1
+			end
+			local npos = visionLib.Table.Clone(v)
+			npos.y=npos.y-1
+			if acceptable[minetest.get_node(npos).name] and not stc[npos.x.." "..npos.y.." "..npos.z] then
+				stc[npos.x.." "..npos.y.." "..npos.z]=visionLib.Table.Clone(npos)
+				table.insert(l2, visionLib.Table.Clone(npos))
+				con=con+1
+			end
 		end
 	end
 	
@@ -44,5 +58,5 @@ function visionLib.Schem.GetConnected(pos, acceptable)
 		stc[k].name=minetest.get_node(v).name
 	end
 	
-	return(stc)
+	return stc
 end
